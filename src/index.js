ffaf17b059e45a00f66bfada3f9c11e987dfe16e
Toon Derkoningen <toon.derkoningen@gmail.com>
2019-01-04 17:27:38 +0100

    props from fixtures now get passed in as initial props

--- src/index.js
+++ src/index.js
@@ -1,64 +1,78 @@
 import React from 'react';
-const deepMerge = require('deepmerge'); // TODO: find a package that is bundled with modern standards
+const deepMerge = require('deepmerge'); // TODO: find a package that is bundled with more modern standards
+
 
 export default (props) => {
     const { nextProxy, ...rest } = props;
     const { value: NextProxy, next } = nextProxy;
+
+    // Perfomance gainz when this proxy is loaded but no controllers fixture is present
     if (!props.fixture.controllers) {
         // Carry on
         return <NextProxy {...rest} nextProxy={next()} />;
     }
 
+    // Extract controllers fixture props
+    const controllers = rest.fixture.controllers
+    // Remove them from the "normal" props
+    rest.fixture.controllers = undefined
+
     // Wrap the component
     return (
         <Wrapper
-            {...props.fixture.controllers}
+            {...rest.fixture.props}
+            controllers={controllers}
             update={props.onFixtureUpdate}
-            Child={state => <NextProxy {...addInnerProps(rest, state)} nextProxy={next()} />}
+            Child={enrichedProps => {
+                // inject enrichedprops in fixture.props
+                const newProps = {
+                    ...rest,
+                    fixture: {
+                        ...rest.fixture,
+                        props: deepMerge(rest.fixture.props, enrichedProps),
+
+                    },
+                }
+                return (
+                    <NextProxy {...newProps} nextProxy={next()} />
+                )
+            }}
         />
     )
 };
-
-function addInnerProps(props, add) {
-    return {
-        ...props,
-        fixture: {
-            ...props.fixture,
-            props: {
-                ...props.fixture.props,
-                ...add
-            }
-        }
-    }
-}
 
 class Wrapper extends React.Component {
     static hoc = true;
     constructor(props) {
         super(props);
         // Extract controller props and react-cosmos update function
-        const { Child, update, ...controllers } = props;
-        this.state = this.extractControllersFunctionsObject(controllers, update, (...args) => this.deepMergeState(...args));
+        const { update, controllers } = props;
+        this.state = {
+            ...this.extractControllersFunctionsObject(controllers, update, (...args) => this.deepMergeState(...args))
+        };
     }
 
     // To prevent overwriting nested props we set in a fixture
     deepMergeState = (nextState) => {
-        this.setState(s => deepMerge(s, nextState))
+        this.setState(s => deepMerge(s, nextState));
     }
 
     // For recursively transforming controllers prop to state setting functions
     extractControllersFunctionsObject(controllers, update, setState) {
         const transformedControllerObject = Object.entries(controllers).map(([key, value]) => {
+            // type == object: branch
             if (typeof value === 'object') {
                 return {
                     [key]: this.extractControllersFunctionsObject(value, update, setState)
                 };
             }
+            // type == function: leaf
             else if (typeof value === 'function') {
                 return {
                     [key]: (...args) => {
                         // Pass in controller function args
-                        const nextState = value(...args) || {}
+                        // Set empty object as fallback when there is no return value provided by the user
+                        const nextState = value(...args) || {};
                         // Update editor
                         update(nextState);
                         // Update state
@@ -66,6 +80,7 @@ class Wrapper extends React.Component {
                     }
                 };
             }
+            // type == something else: user isn't making sense
             else {
                 return {
                     [key]: () => { console.warn(`Did you provide an object to 'controllers' proxy that doesn't strictly have functions as object tree leaves?`) }
@@ -78,8 +93,10 @@ class Wrapper extends React.Component {
     }
 
     render() {
-        const { Child, update, ...rest } = this.props;
-        const props = { ...rest, ...this.state };
+        // Get all props without Child, update and controllers
+        const { Child, update, controllers, ...rest } = this.props;
+        // Prevent nested props from being overwritten when being merged with our generated state
+        const props = deepMerge(rest, this.state);
         return <Child {...props} />
     }
 }
